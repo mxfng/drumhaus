3838a7bdaf6fed71fa6d4dfac19759cb4751bff8
mxfng <maxhfung@gmail.com>
2023-11-13 13:15:27 -0800

    scaler experiment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Scaler from "@/components/Scaler";
 import Drumhaus from "../components/Drumhaus";
+import { Center } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
-      <Drumhaus />
+      <Center h="100vh" w="100vw" overflow="hidden" bg="gray">
+        <Drumhaus />
+      </Center>
     </>
   );
 }
