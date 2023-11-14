e6484dbf6675036c8cd88f6e5e5a88547729768d
mxfng <maxhfung@gmail.com>
2023-11-13 18:32:32 -0800

    add some knobs

--- src/components/Slot.tsx
+++ src/components/Slot.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SlotData } from "@/types/types";
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 import "@fontsource-variable/pixelify-sans";
 import { Knob } from "./Knob";
 import { useEffect, useRef, useState } from "react";
@@ -14,13 +14,22 @@ type SlotParams = {
 
 export const Slot: React.FC<SlotParams> = ({ data }) => {
   const [volume, setVolume] = useState(90); // 0-100
+  const [attack, setAttack] = useState(0);
+  const [release, setRelease] = useState(0);
+
   const [waveWidth, setWaveWidth] = useState<number>(100);
 
-  const waveButtonRef = useRef<HTMLButtonElement>(null);
+  // Control attack
+  useEffect(() => {
+    const newAttackValue = transformKnobValue(attack, [0, 1]);
+    data.sampler.sampler.attack = newAttackValue;
+  });
 
-  const playSample = () => {
-    data.sampler.sampler.triggerAttack("C2");
-  };
+  // Control attack
+  useEffect(() => {
+    const newReleaseValue = transformKnobValue(release, [0, 1]);
+    data.sampler.sampler.release = newReleaseValue;
+  });
 
   // Control volume
   useEffect(() => {
@@ -54,6 +63,12 @@ export const Slot: React.FC<SlotParams> = ({ data }) => {
     return scalingFactor * input + newRangeMin;
   };
 
+  const waveButtonRef = useRef<HTMLButtonElement>(null);
+
+  const playSample = () => {
+    data.sampler.sampler.triggerAttack("C2");
+  };
+
   return (
     <>
       <Box w="100%" key={`Slot-${data.name}`} p={4}>
@@ -80,13 +95,35 @@ export const Slot: React.FC<SlotParams> = ({ data }) => {
           <Waveform audioFile={data.sampler.url} width={waveWidth} />
         </Button>
 
-        <Knob
-          key={`knob1-${data.name}`}
-          size={60}
-          knobValue={volume}
-          setKnobValue={setVolume}
-        />
-        <Text>{volume}</Text>
+        <Grid templateColumns="repeat(2, 1fr)">
+          <GridItem>
+            <Knob
+              key={`knob-${data.name}-attack`}
+              size={60}
+              knobValue={attack}
+              setKnobValue={setAttack}
+            />
+            <Text>Attack: {attack}</Text>
+          </GridItem>
+          <GridItem>
+            <Knob
+              key={`knob-${data.name}-release`}
+              size={60}
+              knobValue={release}
+              setKnobValue={setRelease}
+            />
+            <Text>Release: {release}</Text>
+          </GridItem>
+          <GridItem>
+            <Knob
+              key={`knob-${data.name}-volume`}
+              size={60}
+              knobValue={volume}
+              setKnobValue={setVolume}
+            />
+            <Text>Volume: {volume}</Text>
+          </GridItem>
+        </Grid>
       </Box>
 
       {/* Divider Line */}
