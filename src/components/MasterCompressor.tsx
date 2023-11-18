c1ca4c8d12aa000e7f8041e9c457206ab2ebacbd
mxfng <maxhfung@gmail.com>
2023-11-17 20:00:23 -0800

    compressor

---
+++ src/components/MasterCompressor.tsx
@@ -0,0 +1,53 @@
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Knob } from "./Knob";
+
+type MasterCompressorProps = {
+  threshold: number;
+  setThreshold: React.Dispatch<React.SetStateAction<number>>;
+  ratio: number;
+  setRatio: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const MasterCompressor: React.FC<MasterCompressorProps> = ({
+  threshold,
+  setThreshold,
+  ratio,
+  setRatio,
+}) => {
+  return (
+    <Box h="100%" w="120px">
+      <Grid templateColumns="repeat(2, 1fr)">
+        <GridItem position="relative">
+          <Text
+            transform="rotate(-90deg)"
+            position="absolute"
+            left={-5}
+            bottom="70px"
+            color="gray"
+            fontSize={12}
+          >
+            COMPRESSOR
+          </Text>
+        </GridItem>
+        <GridItem>
+          <Knob
+            size={60}
+            knobValue={threshold}
+            setKnobValue={setThreshold}
+            knobTitle="THRESHOLD"
+            knobTransformRange={[-30, 0]}
+            knobUnits="dB"
+          />
+          <Knob
+            size={60}
+            knobValue={ratio}
+            setKnobValue={setRatio}
+            knobTitle="RATIO"
+            knobTransformRange={[1, 8]}
+            knobUnits=": 1"
+          />
+        </GridItem>
+      </Grid>
+    </Box>
+  );
+};
