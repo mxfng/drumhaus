ace154ad2db58e9cb923b8147e1968ff31141767
mxfng <maxhfung@gmail.com>
2023-11-18 00:19:07 -0800

    velocities

--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -1,17 +1,29 @@
 "use client";
 
 import { Sequences } from "@/types/types";
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Center, Grid, GridItem, Text } from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
 
 const STEP_BOXES_GAP = 12;
 const NUM_OF_STEPS = 16;
 
-export const Sequencer: React.FC<any> = ({
+type SequencerProps = {
+  sequence: boolean[];
+  setSequence: React.Dispatch<React.SetStateAction<boolean[]>>;
+  sequences: Sequences;
+  setSequences: React.Dispatch<React.SetStateAction<Sequences>>;
+  variation: number;
+  slot: number;
+  step: number;
+  isPlaying: boolean;
+};
+
+export const Sequencer: React.FC<SequencerProps> = ({
   sequence,
-  variation,
   setSequence,
+  sequences,
   setSequences,
+  variation,
   slot,
   step,
   isPlaying,
@@ -19,7 +31,7 @@ export const Sequencer: React.FC<any> = ({
   const [parentWidth, setParentWidth] = useState<number>(0);
   const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
   const [isWriting, setWriteState] = useState<boolean>(true);
-
+  const [isVelocity, setIsVelocity] = useState<boolean>(false);
   const sequencerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -67,6 +79,7 @@ export const Sequencer: React.FC<any> = ({
       setSequences((prevSequences: Sequences) => {
         const newSequences = [...prevSequences];
         newSequences[slot][variation][0] = newSequence;
+        newSequences[slot][variation][1][index] = 1;
         return newSequences;
       });
 
@@ -81,13 +94,48 @@ export const Sequencer: React.FC<any> = ({
   };
 
   const toggleStepOnMouseOver = (node: number, nodeState: boolean) => {
-    if (isMouseDown && nodeState !== isWriting) {
+    if (isMouseDown && nodeState !== isWriting && !isVelocity) {
       toggleStep(node);
+    }
+  };
+
+  const getVelocityValue = (
+    event: React.MouseEvent<HTMLDivElement>,
+    node: number
+  ) => {
+    const targetDiv = event.currentTarget;
+    const rect = targetDiv.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const divWidth = rect.width;
+    const calculateVelocity = Math.max(Math.min(mouseX / divWidth, 100), 0);
+    setSequences((prevSequences) => {
+      const newSequences = [...prevSequences];
+      newSequences[slot][variation][1][node] = calculateVelocity;
+      return newSequences;
+    });
+  };
+
+  const adjustVelocityOnMouseDown = (
+    event: React.MouseEvent<HTMLDivElement>,
+    node: number
+  ) => {
+    setIsMouseDown(true);
+    setIsVelocity(true);
+    getVelocityValue(event, node);
+  };
+
+  const adjustVelocityOnMouseMove = (
+    event: React.MouseEvent<HTMLDivElement>,
+    node: number
+  ) => {
+    if (isMouseDown && isVelocity) {
+      getVelocityValue(event, node);
     }
   };
 
   const handleMouseUp = () => {
     setIsMouseDown(false);
+    setIsVelocity(false);
   };
 
   return (
@@ -133,6 +181,42 @@ export const Sequencer: React.FC<any> = ({
                 }}
                 transform="0.2s ease"
               />
+              <Box
+                key={`sequenceNodeVelocity${node}`}
+                w="100%"
+                h="14px"
+                mt={3}
+                bg={sequence[node] ? "transparent" : "transparent"}
+                transition="all 0.2s ease"
+                opacity={sequence[node] ? 0.6 : 0}
+                outline="1px solid darkorange"
+                transform="opacity 0.2s ease"
+                position="relative"
+                onMouseDown={(ev) => adjustVelocityOnMouseDown(ev, node)}
+                onMouseMove={(ev) => adjustVelocityOnMouseMove(ev, node)}
+                _hover={{
+                  "& p": {
+                    opacity: 1,
+                  },
+                }}
+              >
+                <Box
+                  bg="darkorange"
+                  h="100%"
+                  w={`${sequences[slot][variation][1][node] * 100}%`}
+                  position="absolute"
+                />
+                <Center position="absolute" h="100%" w="100%">
+                  <Text
+                    color="white"
+                    fontFamily={`'Pixelify Sans Variable', sans-serif`}
+                    opacity={0}
+                    transition="0.5s ease"
+                  >
+                    {(sequences[slot][variation][1][node] * 100).toFixed(0)}
+                  </Text>
+                </Center>
+              </Box>
             </GridItem>
           )
         )}
