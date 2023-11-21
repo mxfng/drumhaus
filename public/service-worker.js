26fd3866d64b76cd07d7790c291c50ff8d0443d5
mxfng <maxhfung@gmail.com>
2023-11-21 15:20:35 -0800

    add favicon and service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      console.log(`Response returned: ${response}`)
+      return response || fetch(event.request);
+    })
+  );
+});
