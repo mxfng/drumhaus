885efb302a70209bd730e1e1cefd7619b8cfe8ab
mxfng <maxhfung@gmail.com>
2023-11-21 02:14:19 -0800

    rm venv, idiot

--- venv/lib/python3.11/site-packages/soxr/_csoxr_version.c
+++
@@ -1,3 +0,0 @@
-
-#include "csoxr_version.h"
-const char * libsoxr_version() { return "0.1.3-7-gacc0dac"; }
--- venv/lib/python3.11/site-packages/soxr/csoxr_version.c
+++
@@ -1,6 +0,0 @@
-#include <soxr.h>
-#include "csoxr_version.h"
-
-const char * libsoxr_version() {
-    return soxr_version();
-}
