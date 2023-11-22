9c75cd2f9c012d0e5ccfbc393a31b58983b6f5df
mxfng <maxhfung@gmail.com>
2023-11-22 14:01:50 -0800

    try to fix og:image and fade in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 import { Box } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 // Dynamically import Drumhaus component without SSR
 const Drumhaus = dynamic(() => import("../components/Drumhaus"), {
   ssr: false,
 });
 
+function DrumhausFallback() {
+  return <></>;
+}
+
 export default function Home() {
   return (
     <Box w="100%" h="100%">
       <Box w="100%" h="100vh" minW={1538} minH={1050} position="relative">
-        <Drumhaus />
+        <Suspense fallback={<DrumhausFallback />}>
+          <Drumhaus />
+        </Suspense>
       </Box>
     </Box>
   );
