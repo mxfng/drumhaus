d3ad45c744ab5d47b0974f815af1784e36b0f622
mxfng <maxhfung@gmail.com>
2025-11-25 13:26:38 -0800

    chore: Fix slider track color

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -10,8 +10,8 @@ export const Slider = forwardRef<
     className={`relative flex w-full touch-none items-center select-none ${className ?? ""} `}
     {...props}
   >
-    <SliderPrimitive.Track className="bg-primary shadow-inset relative h-2 w-full grow cursor-pointer overflow-hidden rounded-full">
-      <SliderPrimitive.Range className="bg-primary-muted absolute h-full" />
+    <SliderPrimitive.Track className="bg-background shadow-inset relative h-2 w-full grow cursor-pointer overflow-hidden rounded-full">
+      <SliderPrimitive.Range className="bg-primary absolute h-full" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="shadow-neu-raised focus-visible:ring-primary-muted block h-5 w-5 cursor-pointer rounded-full bg-[linear-gradient(160deg,var(--color-gradient-light),var(--color-gradient-dark))] transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
