708ca320be27303f4802aa404ddaf087b4b3e58c
mxfng <maxhfung@gmail.com>
2025-11-27 01:38:17 -0800

    Add knob sensitivity

--- src/lib/knob/constants.ts
+++ src/lib/knob/constants.ts
@@ -3,3 +3,4 @@ export const KNOB_VALUE_MIN = 0;
 export const KNOB_VALUE_DEFAULT = 50;
 export const KNOB_OUTER_TICK_COUNT_DEFAULT = 2;
 export const KNOB_EXPONENTIAL_CURVE_POWER = 2;
+export const KNOB_SENSITIVITY = 0.9;
