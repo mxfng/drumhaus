406883d39e6724ca396a566f68e4a48759d57d04
mxfng <maxhfung@gmail.com>
2025-11-29 01:39:26 -0800

    Disable touch hold with useTouchHoldBlock

--- src/layout/mobile/DrumhausMobile.tsx
+++ src/layout/mobile/DrumhausMobile.tsx
@@ -1,5 +1,6 @@
 import { MobilePresetMenu } from "@/features/preset/components/MobilePresetMenu";
 import { useScrollLock } from "@/shared/hooks/useScrollLock";
+import { useTouchHoldBlock } from "@/shared/hooks/useTouchHoldBlock";
 import { MobileBottomNav } from "./MobileBottomNav";
 import { MobileContextMenu } from "./MobileContextMenu";
 import { MobileHeader } from "./MobileHeader";
@@ -8,6 +9,7 @@ import { MobileTabView } from "./MobileTabView";
 const DrumhausMobile: React.FC = () => {
   // Mobile-specific UI hooks
   useScrollLock(true);
+  useTouchHoldBlock(true);
 
   return (
     <div className="bg-surface flex h-dvh flex-col overflow-x-hidden overflow-y-hidden overscroll-none">
