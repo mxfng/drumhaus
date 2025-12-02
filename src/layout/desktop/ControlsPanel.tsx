6b251ce35bd6a44d8105f9e3a583e41c1b75d998
mxfng <maxhfung@gmail.com>
2025-12-02 00:17:18 -0800

    Add CLAUDE.md

--- src/layout/desktop/ControlsPanel.tsx
+++ src/layout/desktop/ControlsPanel.tsx
@@ -37,7 +37,7 @@ export const ControlsPanel = () => {
       <TempoControls />
 
       {/* Pattern Controls */}
-      <div className="border-border col-span-2 flex flex-row border-x px-4">
+      <div className="border-border col-span-2 flex h-full flex-row items-center justify-center border-x px-4">
         <SequencerControl />
       </div>
 
@@ -45,7 +45,7 @@ export const ControlsPanel = () => {
       <GrooveControls />
 
       {/* Master Controls */}
-      <HardwareModule className="border-border col-span-2 border-x">
+      <HardwareModule className="border-border col-span-2 flex h-full items-center justify-center border-l">
         <div className="col-span-2 grid grid-cols-4 gap-2">
           <MasterFX />
         </div>
