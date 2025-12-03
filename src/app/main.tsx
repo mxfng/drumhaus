b622de1f44209a6546f9dc1b2e0edc0acead4da7
mxfng <maxhfung@gmail.com>
2025-12-02 16:58:11 -0800

    idk

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 import "./globals.css";
+import "./animation.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
