6bf136fa611cbdd45602a97dd6909b1450449e23
mxfng <maxhfung@gmail.com>
2025-12-05 17:57:59 -0800

    nudge

--- src/layout/ScreenBar.tsx
+++ src/layout/ScreenBar.tsx
@@ -3,7 +3,6 @@ import { cn } from "@/shared/lib/utils";
 interface ScreenBarProps {
   children: React.ReactNode;
   className?: string;
-  variant?: "default" | "compact";
 }
 
 /**
@@ -13,18 +12,17 @@ interface ScreenBarProps {
 export const ScreenBar: React.FC<ScreenBarProps> = ({
   children,
   className,
-  variant = "default",
 }) => {
   return (
-    <div
-      className={cn(
-        "bg-screen-foreground text-screen flex h-5 items-center",
-        variant === "default" && "rounded-tl-full px-2 pt-0.5 pl-4 text-sm",
-        variant === "compact" && "rounded-tl-md rounded-br-md px-1 text-sm",
-        className,
-      )}
-    >
-      {children}
+    <div className="bg-screen-foreground text-screen rounded-tl-full rounded-br-md">
+      <div
+        className={cn(
+          "flex -translate-y-px items-center px-4 text-sm",
+          className,
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 };
